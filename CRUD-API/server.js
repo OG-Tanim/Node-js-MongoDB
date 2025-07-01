eba469bebc44aafce946b92b000ac7d0c9f5813f
og-tanim <tanimahmedshad@gmail.com>
2025-07-01 17:31:45 +0600

    secrets updated

--- CRUD-API/server.js
+++ CRUD-API/server.js
@@ -4,13 +4,9 @@ import app from "./src/app.js";
 // Connect to MongoDB
 const startServer = async () => {
   try {
-    await mongoose
-      .connect(
-        "mongodb+srv://node-crud-api-backend:DUbKVnYDgza3C8DKs@cluster0.vyhfyba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-      )
-      .then(() => {
-        console.log("Connected to MongoDB");
-      });
+    await mongoose.connect(process.env.DB_URI).then(() => {
+      console.log("Connected to MongoDB");
+    });
 
     app.listen(3000, () => {
       console.log("Server is running on port 3000");
